57fcfd3ebb2cadf6940da35481ea53e9e5d6299f
Elizaveta <isabethia7@gmail.com>
2023-06-01 00:56:22 +0300

    reset

--- src/components/Ingredient.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Ingredient({ amount, measurment, name }) {
-    return (
-      <li>
-        {amount} {measurment} {name}
-      </li>
-    );
-}
